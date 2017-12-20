b2f31e7f5d24ba7305db44df95801dcf4583afb4
Bartosz "izdwuut" Konikiewicz <izdwuut@gmail.com>
2017-12-20 14:05:25 +0100

    Document HPComponent

--- core/src/main/java/io/github/izdwuut/yarl/model/components/creatures/HPComponent.java
+++ core/src/main/java/io/github/izdwuut/yarl/model/components/creatures/HPComponent.java
@@ -2,15 +2,35 @@ package io.github.izdwuut.yarl.model.components.creatures;
 
 import com.badlogic.ashley.core.Component;
 
+/**
+ * Hit points component.
+ * 
+ * @author Bartosz "izdwuut" Konikiewicz
+ * @since  2017-11-18
+ */
 public class HPComponent implements Component {
-	private int HP;
-	public HPComponent(int HP) {
-		this.HP = HP;
+	/** Hit points. */
+	private int hp;
+	
+	public HPComponent(int hp) {
+		this.hp = hp;
 	}
+	
+	/**
+	 * Gets hit points.
+	 * 
+	 * @return hit points
+	 */
 	public int getHP() {
-		return HP;
+		return hp;
 	}
-	public void setHP(int hP) {
-		HP = hP;
+	
+	/**
+	 * Sets {@link #hp hp} to a provided value.
+	 * 
+	 * @param hp hit points to set
+	 */
+	public void setHP(int hp) {
+		this.hp = hp;
 	}
 }
