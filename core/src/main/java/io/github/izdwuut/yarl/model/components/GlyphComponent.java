67d9b110200ebaf6fc46da930b645384a946caa5
Bartosz "izdwuut" Konikiewicz <izdwuut@gmail.com>
2017-12-21 17:10:00 +0100

    Documentation
    Document a GlyphComponent.

--- core/src/main/java/io/github/izdwuut/yarl/model/components/GlyphComponent.java
+++ core/src/main/java/io/github/izdwuut/yarl/model/components/GlyphComponent.java
@@ -2,13 +2,25 @@ package io.github.izdwuut.yarl.model.components;
 
 import com.badlogic.ashley.core.Component;
 
+/**
+ * A display character.
+ * 
+ * @author Bartosz "izdwuut" Konikiewicz
+ * @since  2017-12-10
+ */
 public class GlyphComponent implements Component {
+	/** A character that is displayed on a screen */
 	private char glyph;
 	
 	public GlyphComponent(char glyph) {
 		this.glyph = glyph;
 	}
 	
+	/**
+	 * Gets {@link #glyph a glyph}.
+	 * 
+	 * @return {@link #glyph a glyph}
+	 */
 	public char getGlyph() {
 		return glyph;
 	}
