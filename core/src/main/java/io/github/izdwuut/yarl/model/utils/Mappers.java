94204710c47a28eb0251c7a266edeec8c6f193dd
Bartosz "izdwuut" Konikiewicz <izdwuut@gmail.com>
2017-12-26 13:42:24 +0100

    Add missing mappers
    Add mappers: anAttacker, a Defender, an HP.

--- core/src/main/java/io/github/izdwuut/yarl/model/utils/Mappers.java
+++ core/src/main/java/io/github/izdwuut/yarl/model/utils/Mappers.java
@@ -5,6 +5,9 @@ import com.badlogic.ashley.core.ComponentMapper;
 import io.github.izdwuut.yarl.model.components.GlyphComponent;
 import io.github.izdwuut.yarl.model.components.PositionComponent;
 import io.github.izdwuut.yarl.model.components.SizeComponent;
+import io.github.izdwuut.yarl.model.components.combat.AttackerComponent;
+import io.github.izdwuut.yarl.model.components.combat.DefenderComponent;
+import io.github.izdwuut.yarl.model.components.creatures.HPComponent;
 import io.github.izdwuut.yarl.model.components.creatures.MovementComponent;
 import io.github.izdwuut.yarl.model.components.settings.CellSizeComponent;
 import io.github.izdwuut.yarl.model.components.settings.RNGComponent;
@@ -37,4 +40,13 @@ public class Mappers {
 
 	/** {@link io.github.izdwuut.yarl.model.components.GlyphComponent A Glyph} mapper. */
 	public static final ComponentMapper<GlyphComponent> glyph = ComponentMapper.getFor(GlyphComponent.class);
+	
+	/** {@link io.github.izdwuut.yarl.model.components.combat.AttackerComponent An Attacker} mapper. */
+	public static final ComponentMapper<AttackerComponent> attacker = ComponentMapper.getFor(AttackerComponent.class);
+	
+	/** {@link io.github.izdwuut.yarl.model.components.combat.DefenderComponent A Defender} mapper. */
+	public static final ComponentMapper<DefenderComponent> defender = ComponentMapper.getFor(DefenderComponent.class);
+	
+	/** {@link io.github.izdwuut.yarl.model.components.creatures.HPComponent An HP} mapper. */
+	public static final ComponentMapper<HPComponent> hp = ComponentMapper.getFor(HPComponent.class);
 }
