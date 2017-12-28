51f99ec99477de284212dc7c0a00154718f9351b
Bartosz "izdwuut" Konikiewicz <izdwuut@gmail.com>
2017-12-28 13:21:28 +0100

    Refactoring
    Rename a FlyweightCreatureFactory to a CreatureFlyweightFactory.
    Modify the factory to reduce boilerplate code.

--- core/src/main/java/io/github/izdwuut/yarl/model/factories/FlyweightCreatureFactory.java
+++ core/src/main/java/io/github/izdwuut/yarl/model/factories/FlyweightCreatureFactory.java
@@ -1,9 +1,8 @@
 package io.github.izdwuut.yarl.model.factories;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-
-import com.badlogic.ashley.core.Component;
 
 import io.github.izdwuut.yarl.model.components.GlyphComponent;
 import io.github.izdwuut.yarl.model.components.NameComponent;
@@ -17,21 +16,64 @@ import io.github.izdwuut.yarl.model.entities.Creature;
  * @since  2017-12-27
  */
 public abstract class FlyweightCreatureFactory {
-	List<Component> commonSloth;
+	/**
+	 * Common creature names.
+	 */
+	List<NameComponent> names;
 	
-	protected Creature sloth() {
-		if(commonSloth == null) {
-			commonSloth = new ArrayList<Component>();
-			commonSloth.add(new NameComponent("Sloth"));
-			commonSloth.add(new GlyphComponent('S'));
+	/**
+	 * Common creature display characters.
+	 */
+	List<GlyphComponent> glyphs;
+	
+	public FlyweightCreatureFactory() {
+		names = new ArrayList<NameComponent>();
+		glyphs = new ArrayList<GlyphComponent>();
+	}
+	
+	/**
+	 * Gets a flyweight Sloth.
+	 * 
+	 * @return a flyweight Sloth.
+	 */
+	protected Creature sloth() {		
+		return getFlyweight("Sloth", 'S');
+	}
+	
+	/**
+	 * Gets a flyweight creature. Created to reduce boilerplate code.
+	 * Make sure to operate on {@link #names names} and {@link glyphs} from this method only,
+	 * because both name and glyph have to be treated as one!
+	 * 
+	 * @param name a creature's name
+	 * @param glyph a creature's display character
+	 * 
+	 * @return a flyweight creature
+	 */
+	//TODO: wouldn't be efficient for multiple creatures. use a hashmap instead, add a merge method (takes two creatures as parameters).
+	Creature getFlyweight(String name, char glyph) {
+		Iterator<NameComponent> nameIter = names.iterator();
+		Iterator<GlyphComponent> glyphIter = glyphs.iterator();
+		Creature creature = new Creature();
+		
+		while(nameIter.hasNext() && glyphIter.hasNext()) {
+			NameComponent currName = nameIter.next();
+			GlyphComponent currGlyph = glyphIter.next(); 
+			if(currName.getName() == name && currGlyph.getGlyph() == glyph) {
+				creature.add(currName)
+				.add(currGlyph);
+				
+				return creature;
+			}
 		}
 		
-		Creature sloth = new Creature();
+		NameComponent nameComp = new NameComponent(name);
+		GlyphComponent glyphComp = new GlyphComponent(glyph);
+		creature.add(nameComp)
+		.add(glyphComp);
+		names.add(nameComp);
+		glyphs.add(glyphComp);
 		
-		for(Component comp : commonSloth) {
-			sloth.add(comp);
-		}
-		
-		return sloth;
+		return creature;
 	}
 }
