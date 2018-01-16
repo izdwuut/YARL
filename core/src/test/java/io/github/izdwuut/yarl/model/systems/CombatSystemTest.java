c9cb2ec5cdb6f66de90be904fad20faca009f3dd
Bartosz "izdwuut" Konikiewicz <izdwuut@gmail.com>
2018-01-16 11:15:01 +0100

    CombatSystem unit tests (covers addExp())

--- core/src/test/java/io/github/izdwuut/yarl/model/systems/CombatSystemTest.java
+++ core/src/test/java/io/github/izdwuut/yarl/model/systems/CombatSystemTest.java
@@ -3,19 +3,23 @@ package io.github.izdwuut.yarl.model.systems;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 
 import io.github.izdwuut.yarl.model.components.creatures.ArmsComponent;
+import io.github.izdwuut.yarl.model.components.creatures.ExpComponent;
 import io.github.izdwuut.yarl.model.entities.Combat;
 import io.github.izdwuut.yarl.model.entities.Creature;
+import io.github.izdwuut.yarl.model.entities.Exp;
 import io.github.izdwuut.yarl.model.entities.Settings;
 import io.github.izdwuut.yarl.model.entities.World;
 import io.github.izdwuut.yarl.model.factories.CreatureFactory;
@@ -64,7 +68,6 @@ class CombatSystemTest {
 		Settings settings = new SettingsFactory().getSettings();
 		world = new WorldFactory(settings).getWorld();
 		worldSystem = new WorldSystem(world, settings, engine);
-		
 		engine.addSystem(worldSystem);
 		combatSystem = new CombatSystem(engine);
 		engine.addSystem(combatSystem);
@@ -81,6 +84,14 @@ class CombatSystemTest {
 				Coord.get(11, 3),  
 				Coord.get(41, 16),  
 				Coord.get(5, 20));
+	}
+	
+	@BeforeEach
+	void init() {
+		engine.removeAllEntities();
+		for(Coord pos : positions) {
+			engine.addEntity(creatureFactory.random().setPos(pos));
+		}
 	}
 	
 	/**
@@ -105,5 +116,26 @@ class CombatSystemTest {
 			assertNotEquals(entity.getClass(), Combat.class);
 		}
 		assertEquals(Mappers.dungeon.get(world).getCreatureMap().size(), 0);
+	}
+	
+	/**
+	 * Tests that killing monsters yields experience points.
+	 * Covers {@link io.github.izdwuut.yarl.model.systems.CombatSystem#addExp(Creature creature, int exp) addExp}.
+	 */
+	@Test
+	void addExpTest() {
+		List<Exp> exps = new ArrayList<Exp>();
+		for(Entity entity : engine.getEntities()) {
+			exps.add(new Exp(attacker, entity.getComponent(ExpComponent.class).getExp()));	
+		}
+		engine.update(0);
+		Iterator<Exp> expsIter = exps.iterator();
+		Iterator<Entity> entityIter = engine.getEntities().iterator();
+		while(expsIter.hasNext() && entityIter.hasNext()) {
+			Exp exp = expsIter.next();
+			Entity entity = entityIter.next();
+			assertEquals(Mappers.creature.get(exp).getCreature(), attacker);
+			assertEquals(Mappers.exp.get(entity).getExp(), Mappers.exp.get(exp).getExp());
+		}
 	}
 }
